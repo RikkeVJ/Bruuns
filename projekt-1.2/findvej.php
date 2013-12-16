5d75f9284e92275caa1338369b8cf090f63743f7
pernilleml <fyrrekrattet@gmail.com>
2013-12-16 20:00:35 +0100

    ny

--- projekt-1.2/findvej.php
+++ projekt-1.2/findvej.php
@@ -1,75 +1,30 @@
-<<<<<<< HEAD
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-<link rel="stylesheet" href="css/reset.css" />
-<link rel="stylesheet" href="css/style.css" />
-</head>
-
-<body>
-
-	<h2>Find vej</h2>
-       
-       <form>
-<p align="center"><b>Select a Site </b>
-<select id="setit" style="color: #0000FF" size="1" name="test">
-<option value="">Select one</option>
-    <option value="http://www.altavista.com">AltaVista</option>
-    <option value="http://www.yahoo.com">Yahoo</option>
-     <option value="http://www.google.com">Google</option></select>
-     <input type="button" value="Go"
-onclick="window.open(setit.options[setit.selectedIndex].value)">
-</p></form>
-       
-    <p>Bruun's Galleri ligger tæt på Banegården, busstationen og har sin egen p-kælder. <br>
-    Er du i bil, kan du køre via Værkmestergade fra Spanien. Den fører dig direkte ind til parkeringsanlægget med 1000 p-pladser. Udkørsel sker også via Spanien. <br>
-    Du kan komme til alle herlighederne i en fart uanset transportmiddel. Fra Aarhus hovedbanegård er der direkte adgang, hvis du kommer med tog. <br>
-    Hvis du er på cykel, er der placeret overdækkede cykelparkeringspladser i Bruuns Gade og ved DGI-huset. Der er indgang til centeret fra Banegården, DGI-huset, Jægergårdsgade og M.P Bruuns Gade. <br>
-    Adressen er: M.P. Bruunsgade 25 <br><br>
-    Klik på kortet nedenunder og find vej ved hjælp af Google maps</p>
-
-	<div id="pil">
-        	<img src="images/pil2.png"/>
-    </div>
-    
-    <div id="kort">
-        <a target="_blank" href="https://maps.google.dk/maps?f=q&source=s_q&hl=en&geocode=&abauth=52a98c1eVvELu1lGxU-FvluIc9vN3a4qq0c&authuser=0&q=M.+P.+Bruuns+Gade+25,+Aarhus&aq=&vps=3&jsv=471c&sll=56.149159,10.203702&sspn=0.00698,0.021136&vpsrc=0&g=M.+P.+Bruuns+Gade+25,+Aarhus&num=10">
-			<img src="images/kort.png"</a>
-        </a>
-	</div>
-        
-        
-</body>
-</html>
-
-=======
 <?php
-	$pagetitle ="Find Vej";
+	$pagetitle ="Find vej";
 	include("includes/header.inc.php");
 ?> 
 
-	<h1>Find vej</h1>
-       
-    <p>Bruun's Galleri ligger tæt på Banegården, busstationen og har sin egen p-kælder. <br>
-    Er du i bil, kan du køre via Værkmestergade fra Spanien. Den fører dig direkte ind til parkeringsanlægget med 1000 p-pladser. Udkørsel sker også via Spanien. <br>
-    Du kan komme til alle herlighederne i en fart uanset transportmiddel. Fra Aarhus hovedbanegård er der direkte adgang, hvis du kommer med tog. <br>
-    Hvis du er på cykel, er der placeret overdækkede cykelparkeringspladser i Bruuns Gade og ved DGI-huset. Der er indgang til centeret fra Banegården, DGI-huset, Jægergårdsgade og M.P Bruuns Gade. <br>
-    Adressen er: M.P. Bruunsgade 25 <br><br>
-    Klik på kortet nedenunder og find vej ved hjælp af Google maps</p>
+<body>
+
+	<h1 id="findvejoverskrift">Find vej</h1>
+ 
+    <p class="findvejtekst">Bruun's Galleri ligger tæt på Banegården, busstationen og har sin egen p-kælder.</p>
+    <p class="findvejtekst">Er du i bil, kan du køre via Værkmestergade fra Spanien. Den fører dig direkte ind til parkeringsanlægget med 1000 p-pladser. Udkørsel sker også via Spanien.</p>
+    <p class="findvejtekst">Du kan komme til alle herlighederne i en fart uanset transportmiddel. Fra Aarhus hovedbanegård er der direkte adgang, hvis du kommer med tog.</p>
+    <p class="findvejtekst">Hvis du er på cykel, er der placeret overdækkede cykelparkeringspladser i Bruuns Gade og ved DGI-huset.</p>
+    <p class="findvejtekst">Der er indgang til centeret fra Banegården, DGI-huset, Jægergårdsgade og M.P Bruuns Gade.</p>
+    <p class="findvejtekst">Adressen er: M.P. Bruunsgade 25 </p>
+    <p class="findvejtekst">Klik på kortet nedenunder og find vej ved hjælp af Google maps</p>
 
 	<div id="pil">
         	<img src="images/pil2.png"/>
     </div>
     
-    <div id="kort">
+    <div>
         <a target="_blank" href="https://maps.google.dk/maps?f=q&source=s_q&hl=en&geocode=&abauth=52a98c1eVvELu1lGxU-FvluIc9vN3a4qq0c&authuser=0&q=M.+P.+Bruuns+Gade+25,+Aarhus&aq=&vps=3&jsv=471c&sll=56.149159,10.203702&sspn=0.00698,0.021136&vpsrc=0&g=M.+P.+Bruuns+Gade+25,+Aarhus&num=10">
-			<img src="images/kortoverbruuns.png" alt="link til kort"/>
+			<img id="mapskort" src="images/kortoverbruuns.png"</a>
         </a>
 	</div>
+        
 <?php
 	include("includes/footer.inc.php");
 ?>
-
->>>>>>> 875c3bb42171ea801b59f52b40a60eaa8a93ebee
